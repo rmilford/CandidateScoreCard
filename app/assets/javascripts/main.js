451a3aae95452fbaf80e619d7f12593e6a848c61
Rebekah Milford <rmilford86@gmail.com>
2016-07-07 01:21:19 -0400

    API for Congress functional

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 var url = 'https://congress.api.sunlightfoundation.com/legislators?state=';
 var apiKey = '&apikey=';
 console.log(state)
+
 $("button").click(function(){
   // var search = $('.destination').val();
   // var search = "NJ"
@@ -18,10 +19,21 @@ $("button").click(function(){
     console.log(response);
     members = '';
      for (var i=0;i<response.results.length;i++) {
-       var members = members + '<li>' + response.results[i].first_name + ' ' + response.results[i].last_name +'</li>';
+       members = members + '<li><div id="member" href="/candidates/new">' + response.results[i].first_name + ' ' + response.results[i].last_name +'</div></li>';
      }
 
      $('#output').html(members);
+     $('#candidate_first_name').val('test');
+     $("div").click(function(){
+       i = $('div').index($(this));
+       $('#candidate_first_name').val(response.results[i].first_name);
+       $('#candidate_last_name').val(response.results[i].last_name);
+       $('#candidate_jurisdiction').val(response.results[i].state+' District '+response.results[i].district);
+       $('#candidate_party').val(response.results[i].party);
+       $('#candidate_incumbent').prop('checked', true);
+
+     });
+
   }).fail(function(response){
     console.log("sucks for you", response);
   });
